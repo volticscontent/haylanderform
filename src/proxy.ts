dfff439ad10c41a036c3f955880fdf07af025376
Gustavo <gustavosales99999@gmail.com>
2025-12-23 02:29:17 -0300

    fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const url = req.nextUrl.clone()
   const pathname = url.pathname
 
