64d6341db53d8d96ea24f4ec295d491c9963a640
volticscontent <gustavosales99999@gmail.com>
2026-01-09 19:54:16 -0300

    fox

--- src/lib/docs-content.ts
+++
@@ -1,1 +0,0 @@
-export { docsContent } from './docs'
--- src/types/client.ts
+++
@@ -1,8 +0,0 @@
-export interface SerproClient {
-  id?: string | number
-  nome: string
-  cnpj: string
-  data_ultima_consulta: string | Date | null
-  procuracao_ativa: boolean
-  procuracao_validade?: string | Date | null
-}
